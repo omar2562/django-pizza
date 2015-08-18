3442d09508ae3febe6c811ea35fe2568f6f4eedf
omar2562 <omar2562@gmail.com>
2015-08-18 11:41:46 -0300

    print and interate

--- pyexamples/functions.py
+++ pyexamples/functions.py
@@ -1,15 +1,21 @@
 pedidos = []
 
 
-def adiciona_pedido(nome, sabor):
+def adiciona_pedido(nome, sabor, observacao=None):
     pedido = {}
     pedido['nome'] = nome
     pedido['sabor'] = sabor
+    pedido['observacao'] = observacao
 
-    pedidos.append(pedido)
+    return pedido
 
 
-print (pedidos)
-adiciona_pedido('omar', 'jawaiana')
-adiciona_pedido('mary', 'napolitana')
-print (pedidos)
+pedidos.append(adiciona_pedido('omar', 'jawaiana'))
+pedidos.append(adiciona_pedido('mary', 'napolitana', 'pouco queijo'))
+
+template = '\nNome: {nome}\nSabor: {sabor}'
+for ped in pedidos:
+    print(template.format(**ped))
+    if ped['observacao']:
+        print('Observacao: {0}'.format(ped['observacao']))
+    print('-'*20)    
