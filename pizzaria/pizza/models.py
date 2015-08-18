673906c2aa37281b489a469e51c401c302237a34
omar2562 <omar2562@gmail.com>
2015-08-18 16:40:25 -0300

    upload, no funciona bien

--- pizzaria/pizza/models.py
+++ pizzaria/pizza/models.py
@@ -4,6 +4,7 @@ class Pizza(models.Model):
     name = models.CharField(max_length=128)
     price = models.DecimalField(decimal_places=2, max_digits=5)
     ingredients = models.TextField()
+    image = models.ImageField(null=True, blank=True)
 
     def __unicode__(self):
         return u'Pizza de {}'.format(self.name)
