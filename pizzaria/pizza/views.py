ef6cfafc4b29567e1814a5a644bf4173e7976c01
omar2562 <omar2562@gmail.com>
2015-08-18 17:26:56 -0300

    ainda tem erros, hahaha

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Pizza
 
 def menu(request):
     pizzas = Pizza.objects.all()
-    result = ''
-    for pizza in pizzas:
-        result += pizza.name
-        result += '<br>'
-    return HttpResponse(result)
+    context = { 'pizzas': pizzas }
+    return render(request, 'pizza/menu.html', context)
