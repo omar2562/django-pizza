c12cb5a6e6cded5cb839854c5aa26533ddeeef2c
omar2562 <omar2562@gmail.com>
2015-08-18 15:38:23 -0300

    confir modificacion pizza screen

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Pizza
 
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ingredients']
+    list_display = ['name', 'ingredients']
 
-admin.site.register(Pizza)
+admin.site.register(Pizza, PizzaAdmin)
