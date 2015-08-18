7a43436987b1bff50aeb31da577ad247e74506c0
omar2562 <omar2562@gmail.com>
2015-08-18 09:40:18 -0300

    added condition and multi-line sring

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,2 +1,14 @@
 
 print('Hello World')
+
+salse = 'tomato'
+if salse.startswith('toma'):
+    print("it's probably tomato...")
+else:
+    print("not tomato")
+
+order_description = """
+The client ask for a pizza with less cheese.
+Intead of cheese, he would like to have more jam
+"""
+print(order_description)
