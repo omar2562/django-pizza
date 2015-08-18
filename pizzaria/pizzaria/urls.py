fd1171aa2b5f76f65fb2cb426f0f91e64dd88d24
omar2562 <omar2562@gmail.com>
2015-08-18 16:51:35 -0300

    link a menu

--- pizzaria/pizzaria/urls.py
+++ pizzaria/pizzaria/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include, url, static
 from django.contrib import admin
+from pizza.views import menu
 
 urlpatterns = [
+    url(r'^menu/', menu),
     url(r'^admin/', include(admin.site.urls)),
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
